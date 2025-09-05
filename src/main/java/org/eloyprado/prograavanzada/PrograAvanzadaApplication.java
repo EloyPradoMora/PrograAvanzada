18bf7d6c4e43d15a41000904c3f8993b64534fdc
EloyPradoMora <e.prado02@ufromail.cl>
2025-09-05 10:51:52 -0400

    PrimerPush

---
+++ src/main/java/org/eloyprado/prograavanzada/PrograAvanzadaApplication.java
@@ -0,0 +1,13 @@
+package org.eloyprado.prograavanzada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrograAvanzadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrograAvanzadaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/eloyprado/prograavanzada/PrograAvanzadaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eloyprado.prograavanzada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrograAvanzadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
