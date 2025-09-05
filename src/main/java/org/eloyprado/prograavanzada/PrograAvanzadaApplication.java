3ebff07d722b09d0628d119c700133579a0a9832
YumiGaming <benjamindamian11032006@gmail.com>
2025-09-05 11:43:45 -0400

    HolaMundo
    Despues de runear el codigo hay que abrir localhost:8080 en el navegador para mostrar el holamundo

--- src/main/java/org/eloyprado/prograavanzada/PrograAvanzadaApplication.java
+++ src/main/java/org/eloyprado/prograavanzada/PrograAvanzadaApplication.java
@@ -2,6 +2,8 @@ package org.eloyprado.prograavanzada;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PrograAvanzadaApplication {
@@ -10,4 +12,11 @@ public class PrograAvanzadaApplication {
         SpringApplication.run(PrograAvanzadaApplication.class, args);
     }
 
+    @RestController
+    static class HolaMundoController {
+        @GetMapping("/")
+        public String holaMundo() {
+            return "Hola Mundo";
+        }
+    }
 }
