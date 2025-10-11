d8262732f0bb9e488ccd63c6bbaa93cc624e7a8a
EloyPradoMora <e.prado02@ufromail.cl>
2025-10-11 18:17:49 -0300

    Implementado inicio de sesion regular y con github
    Se ve mas o menos feo, pero se puede iniciar sesion insertando a un usuario por defecto y con la cuenta de github.

--- src/main/java/org/eloyprado/prograavanzada/controller/HomeController.java
+++ src/main/java/org/eloyprado/prograavanzada/controller/HomeController.java
@@ -1,9 +1,11 @@
-package org.eloyprado.prograavanzada;
+package org.eloyprado.prograavanzada.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import usuario.Producto;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,8 +23,17 @@ public class HomeController {
         List<Producto> productos = new ArrayList<Producto>();
         productos.add(new Producto("1", "Acer Nitro 5", 600000));
         productos.add(new Producto("2", "Polera niño", 1000));
-        productos.add(new Producto("3", "Desodorante", 4000));
+        productos.add(new Producto("3", "De sodorante", 4000));
         model.addAttribute("productos", productos);
         return "inicio";
+    }
+
+    @GetMapping("/login")
+    public String login(@RequestParam(value = "error", required = false)String error, Model model){
+        if (error != null) {
+            model.addAttribute("loginError", true);
+            System.out.println("login error");
+        }
+        return "login";
     }
 }
