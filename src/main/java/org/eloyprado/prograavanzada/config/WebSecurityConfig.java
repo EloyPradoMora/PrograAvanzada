3092133fcb17794e8c190b02be9985314593e375
EloyPradoMora <e.prado02@ufromail.cl>
2025-12-10 10:46:02 -0300

    Cierre de sesion agregado

--- src/main/java/org/eloyprado/prograavanzada/config/WebSecurityConfig.java
+++ src/main/java/org/eloyprado/prograavanzada/config/WebSecurityConfig.java
@@ -39,6 +39,12 @@ public class WebSecurityConfig { // Anteriormente 'SecurityConfig'
                         .loginPage("/login")
                         .successHandler(successHandler)
                         .permitAll())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                        .permitAll())
                 .csrf(csrf -> csrf.disable())
                 .build();
     }
